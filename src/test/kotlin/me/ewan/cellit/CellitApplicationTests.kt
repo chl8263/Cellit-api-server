1931370acb7b7f8e40cab3eec24a264649c1b05d
Ewan <chl8263@naver.com>
2020-04-30 21:33:51 -0700

    Initial commit

---
+++ src/test/kotlin/me/ewan/cellit/CellitApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.ewan.cellit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CellitApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

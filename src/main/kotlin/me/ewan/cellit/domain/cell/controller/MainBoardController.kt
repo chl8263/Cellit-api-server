7e8243176448590c5546ffb58637e38cdc94af6d
Ewan <chl8263@naver.com>
2020-05-21 23:41:14 -0700

    View - Get Cells at MainBoard

--- src/main/kotlin/me/ewan/cellit/domain/cell/controller/MainBoardController.kt
+++ src/main/kotlin/me/ewan/cellit/domain/cell/controller/MainBoardController.kt
@@ -3,8 +3,10 @@ package me.ewan.cellit.domain.cell.controller
 import mu.KLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
 import java.security.Principal
 
 @Controller
@@ -14,13 +16,10 @@ class MainBoardController {
     companion object : KLogging()
 
     @GetMapping
-    fun mainBoardForm(principal: Principal) : String {
-        if(principal == null){
-            logger.info { "Main Board is null" }
-        }else {
-            logger.info { "Main Board ${principal.name}" }
-        }
+    fun mainBoardForm(principal: Principal, model: Model) : ModelAndView {
 
-        return "/mainBoard"
+        model.addAttribute("userName",principal.name)
+
+        return ModelAndView("/mainBoard")
     }
 }
---
+++ src/main/resources/static/common/js/const.js
@@ -0,0 +1,5 @@
+const urlScheme = "http",
+    host = "localhost",
+    port = "8080"
+
+const urlPrefix = urlScheme + host + port

9656a9e1b6e8400998e5e36ad0c4e389b097cf8b
Ewan <chl8263@naver.com>
2020-06-14 21:27:27 -0700

    Setting - Setting CORS at SecurityConfig

--- src/main/kotlin/me/ewan/cellit/global/config/SecurityConfig.kt
+++ src/main/kotlin/me/ewan/cellit/global/config/SecurityConfig.kt
@@ -23,6 +23,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -97,5 +100,19 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
                 .mvcMatchers(HttpMethod.POST, "/api/account").permitAll()
                 .mvcMatchers("/admin**").hasAnyAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
+        http.cors()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource{
+        val configuration = CorsConfiguration()
+        configuration.addAllowedOrigin("http://localhost:8080")
+        configuration.addAllowedHeader("*")
+        configuration.addAllowedMethod("*")
+        configuration.allowCredentials =true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }

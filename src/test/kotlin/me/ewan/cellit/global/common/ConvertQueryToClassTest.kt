8afd5d6ee22e42a130c982f065c0987057b53c0c
Ewan <chl8263@naver.com>
2020-07-01 21:21:27 -0700

    Create test to Fail convert query with invalid query class member

--- src/test/kotlin/me/ewan/cellit/global/common/ConvertQueryToClassTest.kt
+++ src/test/kotlin/me/ewan/cellit/global/common/ConvertQueryToClassTest.kt
@@ -3,6 +3,7 @@ package me.ewan.cellit.global.common
 import me.ewan.cellit.domain.cell.vo.query.CellQuery
 import me.ewan.cellit.global.exception.InvalidQueryException
 import org.assertj.core.api.Assertions.assertThat
+import org.codehaus.jackson.map.exc.UnrecognizedPropertyException
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.Executable
@@ -46,5 +47,18 @@ internal class ConvertQueryToClassTest{
 
         //then
         assertThat(errorMsg).isEqualTo(ConvertQueryToClass.INVALID_COLON_MSG)
+    }
+
+    @Test
+    fun `Fail convert query with invalid query class member`(){
+        //given
+        val query = "cellIDDD%3D3"
+
+        //when
+        val convertQueryToClass = assertThrows(UnrecognizedPropertyException::class.java){ConvertQueryToClass.convert<CellQuery>(query)}
+        val errorMsg = convertQueryToClass.message
+
+        //then
+        assertThat(errorMsg).isNotBlank()
     }
 }
